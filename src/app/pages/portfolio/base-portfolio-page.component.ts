bd09570eedaa9550a9a12b8bd06f1403f1b01564
Denny Kim <dennykim93@gmail.com>
2025-04-18 20:52:24 -0700

    Add close button to lightbox. Add swipe gestures to lightbox. Update portfolio cards to show text on mobile

--- src/app/pages/portfolio/base-portfolio-page.component.ts
+++ src/app/pages/portfolio/base-portfolio-page.component.ts
@@ -15,7 +15,10 @@ export class BasePortfolioPageComponent {
   @Input() materials: string = '';
   @Input() images: ProjectImage[] = [];
 
-  lightboxImage: ProjectImage | null = null;  
+  lightboxImage: ProjectImage | null = null;
+
+  touchStartX = 0;
+  touchEndX = 0;
 
   openLightbox(image: ProjectImage) {
     this.lightboxImage = image;
@@ -32,30 +35,30 @@ export class BasePortfolioPageComponent {
 
   navigateLightbox(direction: 'prev' | 'next') {
     if (!this.lightboxImage) return;
-  
+
     const currentIndex = this.images.findIndex(
       (img) => img.assetFilePath === this.lightboxImage?.assetFilePath
     );
-  
+
     let nextIndex = direction === 'next' ? currentIndex + 1 : currentIndex - 1;
-  
+
     if (nextIndex < 0 || nextIndex >= this.images.length) return;
-  
+
     this.lightboxImage = this.images[nextIndex];
   }
-  
+
   get hasNextImage(): boolean {
     if (!this.lightboxImage) return false;
     const currentIndex = this.images.findIndex(img => img.assetFilePath === this.lightboxImage!.assetFilePath);
     return currentIndex < this.images.length - 1;
   }
-  
+
   get hasPrevImage(): boolean {
     if (!this.lightboxImage) return false;
     const currentIndex = this.images.findIndex(img => img.assetFilePath === this.lightboxImage!.assetFilePath);
     return currentIndex > 0;
   }
-  
+
   @HostListener('window:keydown', ['$event'])
   handleKey(event: KeyboardEvent) {
     if (!this.lightboxImage) return;
@@ -72,4 +75,28 @@ export class BasePortfolioPageComponent {
         break;
     }
   }
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].screenX;
+  }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(event: TouchEvent) {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.handleSwipe();
+  }
+
+  handleSwipe() {
+    const swipeDistance = this.touchEndX - this.touchStartX;
+
+    if (Math.abs(swipeDistance) > 50) { // minimum threshold
+      if (swipeDistance > 0 && this.hasPrevImage) {
+        this.navigateLightbox('prev');
+      } else if (swipeDistance < 0 && this.hasNextImage) {
+        this.navigateLightbox('next');
+      }
+    }
+  }
+
 }
